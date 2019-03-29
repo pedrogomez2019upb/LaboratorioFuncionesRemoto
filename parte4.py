29cef45d70f56ce2c76bd0e07e35080b8ca649b4
Pedro Gómez <pipe22007@gmail.com>
2019-03-29 13:09:14 -0500

    Se modifica el algoritmo para que sea una funcion almost_perfect_number

--- parte4.py
+++ parte4.py
@@ -1,16 +1,17 @@
 #Primero declaramos la función a utilizar
-def perfect_number(n):
+def almost_perfect_number(n):
     #Creamos un contador para que cuente la suma        
     sum = 0
     for i in range(1, n):
         #Va sumando al contador para saber si el residuo es cero
         if(n % i == 0):
             sum = sum + i
-    #Si la suma de los divisores es igual a n entonces es un número perfecto
-    if (sum == n):
-        print("The number is a Perfect number!")
+    #Si la suma de los divisores es menor o igual a mismo numero menor a 3 entonces
+    #el número es casi perfecto
+    if (sum <= (n-3)):
+        print("The number is not almost a perfect number!")
     else:
-        print("The number is not a Perfect number!")
+        print("The number is almost a perfect number!")
 #Se llama la función para ejecutarla
-perfect_number(28)
+almost_perfect_number(20)
 #Desarrollado por Pedro Gómez / ID:000396221
