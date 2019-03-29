13812cd24efd37bd89c78a3e80772c99f5a96a98
Pedro Gómez <pipe22007@gmail.com>
2019-03-29 12:39:57 -0500

    Se añade actualización para que ahora imprima números en vez de un mensaje textual

--- parte3.py
+++ parte3.py
@@ -1,15 +1,22 @@
 #Primero declaramos la función a utilizar
 def is_prime(number):
-    #Se crea un for para números primos y su contador que empice desde cero
-    counter = 0
-    for i in range(1,number+1):
-        if (number% i)==0:
-            counter = counter + 1
-    if counter==2:
-        print ("The number {} is prime. Thanks for using this program.".format(number))
-    else:
-        print ("The number {} is not prime. Thanks for using this program.".format(number))
+    #Se crea un try para que revise errores
+    try:           
+        #Se crea un for para números primos y su contador que empice desde cero
+        counter = 0
+        for i in range(1,number+1):
+            if (number% i)==0:
+                counter = counter + 1
+        if counter==2:
+            #Se imprime 1
+            print ("1")
+        else:
+            #Se imprime 0
+            print ("0")
+    except:
+        #Se imprime -1 para que indique que hubo algún error
+        print("-1")
 
 #Se llama la función para que la ejecute
-is_prime(3)
+is_prime(2)
 #Desarrollado por Pedro Gómez / ID:000396221
