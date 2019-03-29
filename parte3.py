cd1227da49ed9d39be98922c6826a68a189f5c7d
Pedro Gómez <pipe22007@gmail.com>
2019-03-29 12:32:19 -0500

    Se corrige el punto para que en vez de dar un retorno imprima el resultado

--- parte3.py
+++ parte3.py
@@ -1,7 +1,5 @@
 #Primero declaramos la función a utilizar
-def is_prime():
-    #Se solicita el número
-    number=int(input("Hi! We are going to check if your number is prime . Please enter the number: "))
+def is_prime(number):
     #Se crea un for para números primos y su contador que empice desde cero
     counter = 0
     for i in range(1,number+1):
@@ -13,5 +11,5 @@ def is_prime():
         print ("The number {} is not prime. Thanks for using this program.".format(number))
 
 #Se llama la función para que la ejecute
-is_prime()
+is_prime(3)
 #Desarrollado por Pedro Gómez / ID:000396221
